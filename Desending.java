084856dce2de9df9db15ee9d4d4a680cef554b43
Satish Kumar Verma <142597434+satishkumarvermas@users.noreply.github.com>
2024-05-05 00:08:33 +0530

    Add files via upload

---
+++ Desending.java
@@ -0,0 +1,12 @@
+import java.util.*;
+
+class Desending {
+    public static void main(String[] args)
+    {
+        Integer array[] = { 1, 2, 3, 4, 5 };
+
+        Arrays.sort(array, Collections.reverseOrder());
+
+        System.out.println(Arrays.toString(array));
+    }
+}
